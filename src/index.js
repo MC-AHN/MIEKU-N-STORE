ee45784d078b0f838d8540f94e5ce32f8ea7f6ad
Muhammad <mh5458837@gmail.com>
2026-01-29 20:13:30 +0700

    FIXBUG: FIXBUG for deployment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 // src/index.js
-import { serve } from "@hono/node-server";
+import { serve, handle } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { serveStatic } from "@hono/node-server/serve-static";
-import login from "./src/APIs/login.js";
-import register from "./src/APIs/register.js";
-import authMiddleware from "./src/APIs/authMiddleware.js";
-import getProduct from "./src/APIs/getProduct.js";
-import postOrder from "./src/APIs/postOrder.js";
+import login from "./APIs/login.js";
+import register from "./APIs/register.js";
+import authMiddleware from "./APIs/authMiddleware.js";
+import getProduct from "./APIs/getProduct.js";
+import postOrder from "./APIs/postOrder.js";
 
 const app = new Hono();
 app.use("/*", cors());
-app.use("/*", serveStatic({ root: './public' }));
+// -- STATIC FILES --
+if (process.env.NODE_ENV !== 'production') {
+  app.use("/*", serveStatic({ root: './public' }));
+}
 
 // -- API LOGIN --
 app.post("/api/login", login);
@@ -29,4 +32,5 @@ const port = 8002;
 console.log(`Server running at http://localhost:${port}`);
 serve({ fetch: app.fetch, port });
 
-export default app;
+// For serverless deployment
+export default handle(app);
