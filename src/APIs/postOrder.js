a648e4475b83cfd88a18b2d7ddb8dbc1aa12b111
Muhammad <mh5458837@gmail.com>
2026-01-29 07:10:14 +0700

    Refactoring: Post Product Api

---
+++ src/APIs/postOrder.js
@@ -0,0 +1,58 @@
+import * as schema from "./db/schema.js";
+import { eq } from "drizzle-orm";
+
+const postOrder = async (c) => {
+    const { customerName, address, items } = await c.req.json();
+    // items: [{ productId: 1, quantity: 2}]
+
+    try {
+        const result = await db.transaction(async (tx) => {
+            let total = 0;
+
+            // 1. Create Order Header
+            const [newOrder] = await tx.insert(schema.orders).values({
+                customerName, address, totalAmount: "0", status: "pending"
+            }).returning();
+
+            // 2. Process Item
+            for (const item of items) {
+                // Cek Stock
+                const product = await tx.query.products.findFirst({
+                    where: eq(schema.products.id, item.productId)
+                });
+
+                if (!product || product.stock < item.quantity) {
+                    throw new Error(`Stock ${product?.name} not enough`);
+                }
+
+                total += (parseFloat(product.price) * item.quantity);
+
+                // insert item and update stock
+                await tx.insert(schema.orderItems).values({
+                    orderId: newOrder.id,
+                    productId: item.productId,
+                    quantity: item.quantity,
+                    priceAtTime: product.price
+                });
+
+                await tx.update(schema.products)
+                    .set({ stock: product.stock - item.quantity })
+                    .where(eq(schema.products.id, item.productId));
+            }
+
+            // Update Total Price
+            await tx.update(schema.orders)
+                .set({ totalAmount: total })
+                .where(eq(schema.orders.id, newOrder.id));
+
+            return { orderId: newOrder.id, total };
+        });
+
+        return c.json({ success: true, ...result })
+    
+    } catch (e) {
+        return c.json({ success: false, message: e.message }, 400);
+    }
+};
+
+export default postOrder;
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import login from "./APIs/login.js";
 import register from "./APIs/register.js";
 import authMiddleware from "./APIs/authMiddleware.js";
 import getProduct from "./APIs/getProduct.js";
+import postOrder from "./APIs/postOrder.js";
 
 // 1, LOAD ENV
 process.loadEnvFile();
@@ -34,59 +35,7 @@ app.post('/api/products', authMiddleware, register);
 
 app.get('/api/products', getProduct);
 
-app.post('/api/orders', async (c) => {
-    const { customerName, address, items } = await c.req.json();
-    // items: [{ productId: 1, quantity: 2}]
-
-    try {
-        const result = await db.transaction(async (tx) => {
-            let total = 0;
-
-            // 1. Create Order Header
-            const [newOrder] = await tx.insert(schema.orders).values({
-                customerName, address, totalAmount: "0", status: "pending"
-            }).returning();
-
-            // 2. Process Item
-            for (const item of items) {
-                // Cek Stock
-                const product = await tx.query.products.findFirst({
-                    where: eq(schema.products.id, item.productId)
-                });
-
-                if (!product || product.stock < item.quantity) {
-                    throw new Error(`Stock ${product?.name} not enough`);
-                }
-
-                total += (parseFloat(product.price) * item.quantity);
-
-                // insert item and update stock
-                await tx.insert(schema.orderItems).values({
-                    orderId: newOrder.id,
-                    productId: item.productId,
-                    quantity: item.quantity,
-                    priceAtTime: product.price
-                });
-
-                await tx.update(schema.products)
-                    .set({ stock: product.stock - item.quantity })
-                    .where(eq(schema.products.id, item.productId));
-            }
-
-            // Update Total Price
-            await tx.update(schema.orders)
-                .set({ totalAmount: total })
-                .where(eq(schema.orders.id, newOrder.id));
-
-            return { orderId: newOrder.id, total };
-        });
-
-        return c.json({ success: true, ...result })
-    
-    } catch (e) {
-        return c.json({ success: false, message: e.message }, 400);
-    }
-});
+app.post('/api/orders', authMiddleware, postOrder);
 
 // Start Server
 const port = 8002;
